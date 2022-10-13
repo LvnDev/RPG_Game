5162709f69ad675831853e29ebb0b63ab10bc7c8
LVNdev <email>
2022-10-13 12:09:42 +0100

    Add project files.

---
+++ RPG_Game/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RPG_Game
+{
+    class Character
+    {
+        public string Name;
+        public int Health;
+        public int DamageMaximum;
+        public bool AttackBonus;
+
+        public int Attack()
+        {
+            Random random = new Random();
+            int damage = random.Next(DamageMaximum);
+            return damage;
+        }
+
+        public void Defend(int damage)
+        {
+            this.Health = damage;
+        }
+}   }
---
+++ RPG_Game/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RPG_Game
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int damage;
+
+            Character hero = new Character();
+            hero.Name = "Hero";
+            hero.DamageMaximum = 10;
+            hero.AttackBonus = true;
+            hero.Health = 500;
+            Character monster = new Character();
+            monster.Name = "Monster";
+            monster.DamageMaximum = 15;
+            monster.AttackBonus = false;
+            monster.Health = 600;
+
+            while (hero.Health > 0 || monster.Health > 0) 
+            {
+                damage = hero.Attack();
+                monster.Defend(damage);
+
+                damage = monster.Attack();
+                hero.Defend(damage);
+
+                printStats(hero);
+                printStats(monster);
+            }
+
+            
+
+        }
+        private void printStats(Character characeter)
+        {
+            Label1.Text += String.Format("<p>Name: {0} - Health: {1} - Damage Maximum: {2} - Attack Bonus: {3}",
+                characeter.Name,
+                characeter.Health,
+                characeter.AttackBonus.ToString(),
+                characeter.DamageMaximum.ToString()); ;
+
+
+        }
+    }
+}
